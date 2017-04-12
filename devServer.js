4c95d952bd5676523b96f23f24a475afd8d0b14c
Andrew Lachkovics <andrew.lachkovics@azubu.com>
2017-04-11 20:48:25 -0700

    config file additions

--- devServer.js
+++ devServer.js
@@ -12,6 +12,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
+app.use(express.static('public'));
 
 app.use('/public', express.static('public'));
 

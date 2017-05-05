32546a7d6dfdfbd2f73c5c46ce43ea5626c7eaae
Andrew Lachkovics <andrew.lachkovics@quickmobile.com>
2017-05-05 10:48:33 -0700

    added data loading

--- src/components/H1BGraph/index.jsx
+++ src/components/H1BGraph/index.jsx
@@ -1,9 +1,59 @@
 // ./src/components/H1BGraph/index.jsx
 import React, { Component } from 'react';
-import d3 from 'd3';
+import * as d3 from "d3";
 
 class H1BGraph extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            rawData: []
+        };
+    }
+
+    componentWillMount() {
+        this.loadRawData();
+    }
+
+	cleanJobs(name) {
+		return name;
+	}
+
+	loadRawData() {
+		console.log(d3);
+		let dateFormat = d3.timeParse("%m/%d/%Y");
+        d3.csv(this.props.url)
+          .row((d) => {
+			if (!d['base salary']) {
+				return null;
+			}
+			return {employer: d.employer,
+					submit_date: dateFormat(d['submit date']),
+					start_date: dateFormat(d['start date']),
+					case_status: d['case status'],
+					job_title: d['job title'],
+					clean_job_title: this.cleanJobs(d['job title']),
+					base_salary: Number(d['base salary']),
+					salary_to: d['salary to'] ? Number(d['salary to']) : null,
+					city: d.city,
+					state: d.state};
+          })
+          .get((error, rows) => {
+              if (error) {
+                  console.error(error);
+                  console.error(error.stack);
+              } else {
+                  this.setState({rawData: rows});
+              }
+          });
+    }
+
     render() {
+        if (!this.state.rawData.length) {
+            return (
+                <h2>Loading data about 81,000 H1B visas in the software industry</h2>
+            );
+        }
         return (
             <div>
                 <svg>

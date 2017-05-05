3a9574783f644490a14d5f939002fd482bfa769f
Andrew Lachkovics <andrew.lachkovics@azubu.com>
2017-05-04 22:02:22 -0700

    creating hb1graph component

---
+++ src/components/H1BGraph/index.jsx
@@ -0,0 +1,16 @@
+// ./src/components/H1BGraph/index.jsx
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+class H1BGraph extends Component {
+    render() {
+        return (
+            <div>
+                <svg>
+                </svg>
+            </div>
+        );
+    }
+}
+
+export default H1BGraph;

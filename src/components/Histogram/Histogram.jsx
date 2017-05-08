029f1636c1bdb07fe13eb30ec79c705c6bb92f2a
Andrew Lachkovics <andrew.lachkovics@quickmobile.com>
2017-05-08 16:19:14 -0700

    draw histogram @pg107

--- src/components/Histogram/Histogram.jsx
+++ src/components/Histogram/Histogram.jsx
@@ -13,6 +13,22 @@ class Histogram extends Component {
         this.update_d3(props);
     }
 
+	makeBar(bar) {
+        let percent = bar.y / this.props.data.length * 100,
+        	props = {
+    			percent: percent,
+				x: this.props.axisMargin,
+				y: this.yScale(bar.x),
+				width: this.widthScale(bar.y),
+				height: this.yScale(bar.dx),
+				key: "histogram-bar-" + bar.x + "-" + bar.y
+			};
+
+        return (
+            <HistogramBar {...props} />
+        );
+    }
+
     componentWillReceiveProps(newProps) {
         this.update_d3(newProps);
     }
@@ -24,25 +40,59 @@ class Histogram extends Component {
 
         let bars = this.histogram(props.data),
             counts = bars.map((d) => d.y);
-console.log(bars);
         this.widthScale
             .domain([d3.min(counts), d3.max(counts)])
             .range([9, props.width-props.axisMargin]);
 
         this.yScale
-            .domain([0, d3.max(bars.map((d) => d.x+d.dx))])
-            .range([0, props.height-props.topMargin-props.bottomMargin]);
+            .domain([0, d3.max(bars.map((d) => d.x + d.dx))])
+            .range([0, props.height - props.topMargin - props.bottomMargin]);
 	    }
     }
 
     render() {
-        let translate = `translate(0, ${this.props.topMargin})`;
+        let translate = `translate(0, ${this.props.topMargin})`,
+        	bars = this.histogram(this.props.data);
 
         return (
             <g className="histogram" transform={translate}>
+            	<g className="bars">
+            	{bars.map(this.makeBar.bind(this))}
+            	</g>
             </g>
         );
     }
 }
 
 export default Histogram;
+
+class HistogramBar extends Component {
+    render() {
+        let translate = `translate(${this.props.x}, ${this.props.y})`,
+            label = this.props.percent.toFixed(0) + '%';
+
+		if (this.props.percent < 1) {
+			label = this.props.percent.toFixed(2)+"%";
+		}
+		if (this.props.width < 20) {
+			label = label.replace("%", "");
+		}
+		if (this.props.width < 10) {
+			label = "";
+		}
+
+        return (
+            <g transform={translate} className="bar">
+                <rect width={this.props.width}
+                      height={this.props.height - 2}
+                      transform="translate(0, 1)">
+                </rect>
+                <text textAnchor="end"
+                      x={this.props.width - 5}
+                      y={this.props.height / 2 + 3}>
+                    {label}
+                </text>
+            </g>
+        );
+    }
+}
